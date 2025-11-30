59756bfdd894f8b5a3df3d3e102988842b6f6825
August Saint Freytag <me@augustfreytag.com>
2025-11-30 13:57:15 +0100

    Initial commit, add mod skeleton

---
+++ src/main/java/net/saint/patchassortment/Mod.java
@@ -0,0 +1,23 @@
+package net.saint.patchassortment;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Mod implements ModInitializer {
+
+	// Configuration
+
+	public static final String MOD_ID = "patch-assortment";
+
+	// References
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	// Init
+
+	@Override
+	public void onInitialize() {
+	}
+}
