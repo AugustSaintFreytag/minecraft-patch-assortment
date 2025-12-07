dad2c4d7d1bc0a5a43de691e7385bb3172305f23
August Saint Freytag <me@augustfreytag.com>
2025-12-07 18:34:30 +0100

    Rename injected mixin methods for ships

--- src/main/java/net/saint/patchassortment/mixin/smallships/BriggEntityMixin.java
+++ src/main/java/net/saint/patchassortment/mixin/smallships/BriggEntityMixin.java
@@ -1,16 +1,17 @@
 package net.saint.patchassortment.mixin.smallships;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 import com.talhanation.smallships.world.entity.ship.BriggEntity;
-import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(BriggEntity.class)
 public class BriggEntityMixin {
-	
+
 	@Inject(method = "waterSplash", at = @At("HEAD"), cancellable = true, remap = false)
-	private void sspd$waterSplash(CallbackInfo callbackInfo) {
+	private void patchassortment$waterSplash(CallbackInfo callbackInfo) {
 		callbackInfo.cancel();
 	}
 
--- src/main/java/net/saint/patchassortment/mixin/smallships/CogEntityMixin.java
+++ src/main/java/net/saint/patchassortment/mixin/smallships/CogEntityMixin.java
@@ -1,17 +1,17 @@
 package net.saint.patchassortment.mixin.smallships;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import com.talhanation.smallships.world.entity.ship.CogEntity;
-import org.spongepowered.asm.mixin.injection.At;
 
+import com.talhanation.smallships.world.entity.ship.CogEntity;
 
 @Mixin(CogEntity.class)
 public abstract class CogEntityMixin {
-	
+
 	@Inject(method = "waterSplash", at = @At("HEAD"), cancellable = true, remap = false)
-	private void sspd$waterSplash(CallbackInfo callbackInfo) {
+	private void patchassortment$waterSplash(CallbackInfo callbackInfo) {
 		callbackInfo.cancel();
 	}
 
