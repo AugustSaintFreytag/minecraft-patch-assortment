85c95cfa53c4c4f2e8ebd7e979c0bcd6fa684ed7
August Saint Freytag <me@augustfreytag.com>
2026-01-05 16:09:40 +0100

    Update formatting in T.A. sky angle mixin

--- src/main/java/net/saint/patchassortment/mixin/tenseambience/BiomeSoundHandlerSkyAngleMixin.java
+++ src/main/java/net/saint/patchassortment/mixin/tenseambience/BiomeSoundHandlerSkyAngleMixin.java
@@ -9,7 +9,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.cyberking42.tenseambience.BiomeSoundHandler;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.world.ClientWorld;
 
 @Mixin(value = BiomeSoundHandler.class, remap = false)
 public abstract class BiomeSoundHandlerSkyAngleMixin {
@@ -19,12 +18,13 @@ public abstract class BiomeSoundHandlerSkyAngleMixin {
 
 	@Inject(method = "onClientTick", at = @At(value = "FIELD", target = "Lnet/cyberking42/tenseambience/BiomeSoundHandler;newIsNight:Z", opcode = Opcodes.PUTSTATIC, shift = At.Shift.AFTER))
 	private static void patchassortment$useSkyAngleForNight(MinecraftClient client, CallbackInfo ci) {
-		ClientWorld world = client.world;
+		var world = client.world;
+
 		if (world == null) {
 			return;
 		}
 
-		float skyAngle = world.getSkyAngle(1.0f);
+		var skyAngle = world.getSkyAngle(1.0f);
 		newIsNight = skyAngle > 0.25f && skyAngle < 0.75f;
 	}
 }
